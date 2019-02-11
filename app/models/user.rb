ebba31839d9c9a463d3df8f3706c9bad0d25ec3e
JC <conwayje@gmail.com>
2019-02-11 17:21:21 -0500

    User profile page, basic information

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,16 @@ class User < ApplicationRecord
   validates_acceptance_of :terms_of_service, message: "must be accepted"
   validates_acceptance_of :age,              message: "must be over 18"
 
+  validates :username, format: { with: /\A[a-zA-Z]{1}[a-zA-Z0-9 ]{0,22}[a-zA-Z0-9]{1}\Z|\A\Z/,
+                                 message: "must start with a letter, not use punctuation, not end with a space, and be under 24 characters",
+                                 allow_nil: true }
+
   def display_name
-    value = username || [first_name, last_name].join(" ")
-    value.truncate(20)
+    value = username.present? ? username : [first_name, last_name].join(" ")
+    value.truncate(24)
+  end
+
+  def profile_incomplete?
+    about_me.blank? # or profile_picture.blank?
   end
 end
