c2673eed8b355a350403033189c2801c608bef9e
JC <conwayje@gmail.com>
2019-04-04 22:44:10 -0400

    Revert migrations

--- db/migrate/20190404014449_create_announcements.rb
+++ db/migrate/20190404014449_create_announcements.rb
@@ -2,6 +2,7 @@ class CreateAnnouncements < ActiveRecord::Migration[5.2]
   def change
     create_table :announcements do |t|
       t.text :body, null: false
+      # t.text :title, null: false
       t.belongs_to :user, foreign_key: true, null: false
 
       t.timestamps
--- db/migrate/20190404014745_add_has_announcement_to_users.rb
+++ db/migrate/20190404014745_add_has_announcement_to_users.rb
@@ -1,6 +1,7 @@
 class AddHasAnnouncementToUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :has_unread_announcement, :boolean, default: false, null: false
+    # add_column :users, :has_unread_first_announcement, :boolean, default: true, null: false
     add_column :users, :is_admin, :boolean, default: false, null: false
   end
 end
