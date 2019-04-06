d0679909154c99442ef0869e4fe3c4c9d3b07861
JC <conwayje@gmail.com>
2019-04-06 00:09:15 -0400

    Change some of the domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,14 +90,14 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: "indieexchange.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: "indiepubexchange.com" }
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'indieexchange.herokuapp.com',
+    :domain => 'indiepubexchange.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
