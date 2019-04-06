3f6897ecf2b07b7275a3c948ba1e901286dd14dd
JC <conwayje@gmail.com>
2019-04-06 00:28:30 -0400

    Fix mailer issue w/ redirect

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: "indiepubexchange.com" }
+  config.action_mailer.default_url_options = { host: "www.indiepubexchange.com" }
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
