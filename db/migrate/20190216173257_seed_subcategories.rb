01b6ef21884d977e3fee2b2f18bef29d93ec10ac
JC <conwayje@gmail.com>
2019-04-06 21:14:55 -0400

    Show user count

--- db/migrate/20190216173257_seed_subcategories.rb
+++ db/migrate/20190216173257_seed_subcategories.rb
@@ -2,6 +2,7 @@ class SeedSubcategories < ActiveRecord::Migration[5.2]
   def self.up
     Subcategory.create!(category: Category.find_by_title("Writing"),       title: "Ghostwriting",         pricing_type: "word")
     Subcategory.create!(category: Category.find_by_title("Writing"),       title: "Blurb",                pricing_type: "booking")
+    Subcategory.create!(category: Category.find_by_title("Writing"),       title: "Plots",                pricing_type: "booking")
     Subcategory.create!(category: Category.find_by_title("Design"),        title: "Cover Art",            pricing_type: "booking")
     Subcategory.create!(category: Category.find_by_title("Design"),        title: "Promotional Graphic",  pricing_type: "booking")
     Subcategory.create!(category: Category.find_by_title("Design"),        title: "Website",              pricing_type: "booking")
@@ -12,6 +13,7 @@ class SeedSubcategories < ActiveRecord::Migration[5.2]
     Subcategory.create!(category: Category.find_by_title("Promotional"),   title: "Newsletter",           pricing_type: "booking")
     Subcategory.create!(category: Category.find_by_title("Promotional"),   title: "Advertising",         pricing_type: "booking")
     Subcategory.create!(category: Category.find_by_title("Promotional"),   title: "Advanced Reader Copy", pricing_type: "booking")
+    Subcategory.create!(category: Category.find_by_title("Promotional"),   title: "Data Services",        pricing_type: "booking")
     Subcategory.create!(category: Category.find_by_title("Miscellaneous"), title: "Virtual Assistant",    pricing_type: "hour")
     Subcategory.create!(category: Category.find_by_title("Miscellaneous"), title: "Formatting",           pricing_type: "booking")
     Subcategory.create!(category: Category.find_by_title("Miscellaneous"), title: "Other",                pricing_type: "booking")
